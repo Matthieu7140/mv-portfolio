d108059683cbd2f9e8549aeb4d77a19076af927b
M. Vagnon <main.mvagnon@icloud.com>
2024-11-07 17:41:57 +0100

    Font optimization (searching for local first then importing)

--- src/theme.ts
+++ src/theme.ts
@@ -73,7 +73,7 @@ export const global = {
           font-style: normal;
           font-display: swap;
           font-weight: 400;
-          src: url(${marcellusRegular}) format('truetype');
+          src: local("Marcellus-Regular"), url(${marcellusRegular}) format('truetype');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
         @font-face {
@@ -81,7 +81,7 @@ export const global = {
           font-style: normal;
           font-display: swap;
           font-weight: 400;
-          src: url(${delaGothicOneRegular}) format('truetype');
+          src: local("DelaGothicOne-Regular"), url(${delaGothicOneRegular}) format('truetype');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
       `,
